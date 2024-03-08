0ec8ea0bc4d0d42d1a13c683b4abedd728b4ed64
Harshad Paradkar <harshad800@gmail.com>
2024-03-08 15:03:37 +0530

    Create a Console App to test WordDocCreator.

---
+++ WordDocCreatorApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using WordDocCreatorLib;
+
+namespace WordDocCreatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var directory = "./";
+            var wordDocCreator = new WordDocCreator(directory);
+            var fileName = "Test-Document";
+            Console.WriteLine(wordDocCreator.SaveAs(fileName));
+        }
+    }
+}
