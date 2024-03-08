2d11ee4808753baa8059c5357ea0156de6b63c3c
Harshad Paradkar <harshad800@gmail.com>
2024-03-08 14:57:10 +0530

    Create WordDocCreator.cs
    Just an initial stub.

---
+++ WordDocCreatorLib/WordDocCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordDocCreatorLib
+{
+    public class WordDocCreator
+    {
+        private readonly string _directory;
+
+        public WordDocCreator(string directory)
+        {
+            _directory = directory;
+        }
+
+        public string SaveAs(string filename)
+        {
+            var filePath = Path.Combine(_directory, filename);
+
+            return filePath;
+        }
+    }
+}
