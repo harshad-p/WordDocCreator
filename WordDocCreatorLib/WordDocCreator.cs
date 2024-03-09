78c449f7094d9989ed12188f0dde27c1e244b172
Harshad Paradkar <harshad800@gmail.com>
2024-03-09 17:08:11 +0530

    Implement IDisposable pattern to free up Word resources.

--- WordDocCreatorLib/WordDocCreator.cs
+++ WordDocCreatorLib/WordDocCreator.cs
@@ -1,25 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Office.Interop.Word;
+using System.Reflection;
 
 namespace WordDocCreatorLib
 {
-    public class WordDocCreator
+    public class WordDocCreator : IDisposable
     {
-        private readonly string _directory;
+        private object oMissing = Missing.Value;
+        private const string oEndOfDoc = "\\endofdoc"; /* \endofdoc is a predefined bookmark */
 
-        public WordDocCreator(string directory)
+        private _Application oWord;
+        private _Document oDoc;
+        private bool disposedValue;
+
+        public WordDocCreator()
         {
-            _directory = directory;
+            //Start Word and create a new document.
+            oWord = new Application
+            {
+                Visible = false
+            };
+            oDoc = oWord.Documents.Add();
         }
 
-        public string SaveAs(string filename)
+        public WordDocCreator(string templateFilePath)
         {
-            var filePath = Path.Combine(_directory, filename);
+            //Start Word and create a new document.
+            oWord = new Application
+            {
+                Visible = false
+            };
+            oDoc = oWord.Documents.Add(templateFilePath);
+        }
 
+        public string SaveAs(string filePath)
+        {
+            var filePathLocal = Path.Combine(filePath + ".docx");
+            oDoc.SaveAs(filePathLocal);
             return filePath;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    oDoc?.Close();
+                    oWord?.Quit();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~WordDocCreator()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
