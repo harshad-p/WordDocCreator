d0b2ac5e04819f91c0ffea3829dd57495ab23652
Harshad Paradkar <harshad800@gmail.com>
2024-03-12 22:57:24 +0530

    Try creating a model for Word Doc Creator data.

--- WordDocCreatorApp/Program.cs
+++ WordDocCreatorApp/Program.cs
@@ -7,28 +7,68 @@ namespace WordDocCreatorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***Recipe Binder Doc. Creator***");
-
-            var directory = "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\Automated";
-            var fileName = "01 Shengdanyachi Bhaaji - Sarika Nilatkar - A5";
-            var templatePath = "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\Templates\\01 A5 - Kohinoor.dotx";
-
-            var wordDocCreator = new WordDocCreator(templatePath);
+            Console.WriteLine("***Word Doc. Creator***");
 
             var wordTable = new WordTable(3, 2);
             wordTable.AddData(["Shengdane", "Ardha Kilo"]).AddData(["Oil", "2 tbl. spoons"]).AddData(["Salt", "Chavinusar"]);
 
-            wordDocCreator.FillShapeWithImage("Recipe_Image", "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\01 Veg\\01 Shengdanyachi Bhaaji\\IMG_0161.HEIC")
-                .UpdateBookmarkedText("Recipe_Title", "Shengdanyachi Bhaaji")
-                .UpdateBookmarkedText("Recipe_Author", "Sarika Nilatkar")
-                .InsertTable("Ingredients_Table", wordTable)
-                .UpdateBookmarkedText("Procedure", "Shengdane bhajun tyancha kut karun ghyaycha.")
-                .UpdateBookmarkedText("Recipe_Is_Ready", "Shengdanyachi Bhaaji Tayyar");
+            var wordTemplateInput = new WordTemplateInput
+            {
+                Directory = "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\Automated",
+                FileName = "01 Shengdanyachi Bhaaji - Sarika Nilatkar - A5",
+                TemplatePath = "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\Templates\\01 A5 - Kohinoor.dotx",
+                WordTables = new Dictionary<string, WordTable>
+                {
+                    { "Ingredients_Table", wordTable }
+                },
+                Images = new Dictionary<string, string>
+                {
+                    { "Recipe_Image", "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\01 Veg\\01 Shengdanyachi Bhaaji\\IMG_0161.HEIC" }
+                },
+                Texts = new Dictionary<string, Tuple<string, string>>
+                {
+                    { "Recipe_Title", Tuple.Create<string, string>("Shengdanyachi Bhaaji", null) },
+                    { "Recipe_Author", Tuple.Create("Sarika Nilatkar" , "www.google.com")},
+                    { "Procedure", Tuple.Create<string, string>("Shengdane bhajun tyancha kut karun ghyaycha." , null)},
+                    { "Recipe_Is_Ready", Tuple.Create<string, string>("Shengdanyachi Bhaaji Tayyar" , null) }
+                }
+            };
 
-            var fileSavePath = wordDocCreator.SaveAs(directory, fileName, SaveAsDocumentType.DOCX);
+            var wordDocCreator = new WordDocCreator(wordTemplateInput.TemplatePath);
+
+            FillImages(wordDocCreator, wordTemplateInput.Images);
+            FillTables(wordDocCreator, wordTemplateInput.WordTables);
+            FillTexts(wordDocCreator, wordTemplateInput.Texts);
+
+            var fileSavePath = wordDocCreator.SaveAs(wordTemplateInput.Directory, wordTemplateInput.FileName, SaveAsDocumentType.DOCX);
             Console.WriteLine(fileSavePath);
 
             wordDocCreator.Dispose();
         }
+
+        private static void FillTexts(WordDocCreator wordDocCreator, IDictionary<string, Tuple<string, string>> texts)
+        {
+            foreach (var text in texts)
+            {
+                wordDocCreator.UpdateBookmarkedText(text.Key, text.Value.Item1, text.Value.Item2);
+            }
+        }
+
+        private static void FillTables(WordDocCreator wordDocCreator, IDictionary<string, WordTable> wordTables)
+        {
+            foreach (var wordTable in wordTables)
+            {
+                wordDocCreator.InsertTable(wordTable.Key, wordTable.Value);
+            }
+        }
+
+        private static void FillImages(WordDocCreator wordDocCreator, IDictionary<string, string> images)
+        {
+            foreach (var image in images)
+            {
+                wordDocCreator.FillShapeWithImage(image.Key, image.Value);
+            }
+        }
+
     }
 }
---
+++ WordDocCreatorApp/WordTemplateInput.cs
@@ -0,0 +1,20 @@
+﻿using WordDocCreatorLib.Models;
+
+namespace WordDocCreatorApp
+{
+    public class WordTemplateInput
+    {
+        public string Directory { get; set; }
+
+        public string FileName { get; set; }
+
+        public string TemplatePath { get; set; }
+
+        public IDictionary<string, WordTable> WordTables { get; set; }
+
+        public IDictionary<string, string> Images { get; set; }
+
+        public IDictionary<string, Tuple<string, string>> Texts { get; set; }
+
+    }
+}
