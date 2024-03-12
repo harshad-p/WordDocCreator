c21c24ab963ff29c7beed0abd73daede0df3ed9c
Harshad Paradkar <harshad800@gmail.com>
2024-03-12 23:06:52 +0530

    Create method to get sample word document data.

--- WordDocCreatorApp/WordTemplateInput.cs
+++ WordDocCreatorApp/WordTemplateInput.cs
@@ -16,5 +16,34 @@ namespace WordDocCreatorApp
 
         public IDictionary<string, Tuple<string, string>> Texts { get; set; }
 
+        public static WordTemplateInput GetSampleInputs()
+        {
+            var wordTable = new WordTable(3, 2);
+            wordTable.AddData(["Shengdane", "Ardha Kilo"]).AddData(["Oil", "2 tbl. spoons"]).AddData(["Salt", "Chavinusar"]);
+
+            var wordTemplateInput = new WordTemplateInput
+            {
+                Directory = "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\Automated",
+                FileName = "01 Shengdanyachi Bhaaji - Sarika Nilatkar - A5",
+                TemplatePath = "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\Templates\\01 A5 - Kohinoor.dotx",
+                WordTables = new Dictionary<string, WordTable>
+                {
+                    { "Ingredients_Table", wordTable }
+                },
+                Images = new Dictionary<string, string>
+                {
+                    { "Recipe_Image", "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\01 Veg\\01 Shengdanyachi Bhaaji\\IMG_0161.HEIC" }
+                },
+                Texts = new Dictionary<string, Tuple<string, string>>
+                {
+                    { "Recipe_Title", Tuple.Create<string, string>("Shengdanyachi Bhaaji", null) },
+                    { "Recipe_Author", Tuple.Create("Sarika Nilatkar" , "www.google.com")},
+                    { "Procedure", Tuple.Create<string, string>("Shengdane bhajun tyancha kut karun ghyaycha." , null)},
+                    { "Recipe_Is_Ready", Tuple.Create<string, string>("Shengdanyachi Bhaaji Tayyar" , null) }
+                }
+            };
+
+            return wordTemplateInput;
+        }
     }
 }
