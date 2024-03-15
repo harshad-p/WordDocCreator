c01da5de37b312e2e2da2a6544dd59a3c3f7dc99
Harshad Paradkar <harshad800@gmail.com>
2024-03-15 20:38:33 +0530

    Create base classes that provide Shengdanyachi Bhaaji inputs.

---
+++ WordDocCreatorApp/SampleRecipeBinderWordDocumentInputProviderBase.cs
@@ -0,0 +1,47 @@
+﻿using WordDocCreatorLib.Models;
+
+namespace WordDocCreatorApp
+{
+    internal abstract class SampleRecipeBinderWordDocumentInputProviderBase : IWordDocumentInputProvider
+    {
+        public abstract IEnumerable<WordDocumentInput> GetWordDocumentInputs();
+
+        public static IEnumerable<WordDocumentInput> GetRecipeBinderSampleInputs()
+        {
+            return
+            [
+                GetShengdanyachiBhaajiInput()
+            ];
+        }
+
+        public static WordDocumentInput GetShengdanyachiBhaajiInput()
+        {
+            var wordTable = new WordTable(3, 2);
+            wordTable.AddData(["Shengdane", "Ardha Kilo"]).AddData(["Oil", "2 tbl. spoons"]).AddData(["Salt", "Chavinusar"]);
+
+            var wordDocumentInput = new WordDocumentInput
+            {
+                Directory = "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\Automated",
+                FileName = "01 Shengdanyachi Bhaaji - Sarika Nilatkar - A5",
+                WordTables = new Dictionary<string, WordTable>
+                {
+                    { "Ingredients_Table", wordTable }
+                },
+                Images = new Dictionary<string, string>
+                {
+                    { "Recipe_Image", "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\01 Veg\\01 Shengdanyachi Bhaaji\\IMG_0161.HEIC" }
+                },
+                Texts = new Dictionary<string, Tuple<string, string>>
+                {
+                    { "Recipe_Title", Tuple.Create<string, string>("Shengdanyachi Bhaaji", null) },
+                    { "Recipe_Author", Tuple.Create("Sarika Nilatkar" , "www.google.com")},
+                    { "Procedure", Tuple.Create<string, string>("Shengdane bhajun tyancha kut karun ghyaycha." , null)},
+                    { "Recipe_Is_Ready", Tuple.Create<string, string>("Shengdanyachi Bhaaji Tayyar" , null) }
+                }
+            };
+
+            return wordDocumentInput;
+        }
+
+    }
+}
---
+++ WordDocCreatorApp/SampleRecipeBinderWordTemplateInputProviderBase.cs
@@ -0,0 +1,30 @@
+﻿namespace WordDocCreatorApp
+{
+    internal abstract class SampleRecipeBinderWordTemplateInputProviderBase : IWordTemplateInputProvider
+    {
+        private static readonly IWordDocumentInputProvider _wordDocumentInputProvider;
+
+        static SampleRecipeBinderWordTemplateInputProviderBase()
+        {
+            _wordDocumentInputProvider = new SampleRecipeBinderWordDocumentInputProvider();
+        }
+
+        public abstract IEnumerable<WordTemplateInput> GetTemplateInputs();
+
+        public static IEnumerable<WordTemplateInput> GetRecipeBinderSampleInputs()
+        {
+            var recipeBinderInput = GetRecipeBinderSampleInput();
+            return
+            [
+                recipeBinderInput
+            ];
+        }
+
+        public static WordTemplateInput GetRecipeBinderSampleInput()
+        {
+            var recipeBinderTemplateFilePath = "\\\\Mac\\Home\\Documents\\Recipe Binder\\Workspace\\Templates\\01 A5 - Kohinoor.dotx";
+            return new WordTemplateInput(recipeBinderTemplateFilePath, _wordDocumentInputProvider.GetWordDocumentInputs());
+        }
+
+    }
+}
