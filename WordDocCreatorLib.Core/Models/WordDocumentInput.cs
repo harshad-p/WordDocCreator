482cdcc00f59448d910f5dc1b9ff155aeb5f157e
Harshad Paradkar <harshad800@gmail.com>
2024-03-15 21:58:16 +0530

    Move WordDocumentInput class to it's own file.

---
+++ WordDocCreatorLib.Core/Models/WordDocumentInput.cs
@@ -0,0 +1,16 @@
+﻿namespace WordDocCreatorLib.Core.Models
+{
+    public class WordDocumentInput
+    {
+        public string Directory { get; set; }
+
+        public string FileName { get; set; }
+
+        public IDictionary<string, WordTable> WordTables { get; set; }
+
+        public IDictionary<string, string> Images { get; set; }
+
+        public IDictionary<string, Tuple<string, string>> Texts { get; set; }
+
+    }
+}
--- WordDocCreatorLib.Core/Models/WordTemplateInput.cs
+++ WordDocCreatorLib.Core/Models/WordTemplateInput.cs
@@ -22,20 +22,5 @@
         public string TemplateFilePath { get; set; }
         public IEnumerable<WordDocumentInput> WordDocumentInputs { get; set; }
 
-
-    }
-
-    public class WordDocumentInput
-    {
-        public string Directory { get; set; }
-
-        public string FileName { get; set; }
-
-        public IDictionary<string, WordTable> WordTables { get; set; }
-
-        public IDictionary<string, string> Images { get; set; }
-
-        public IDictionary<string, Tuple<string, string>> Texts { get; set; }
-
     }
 }
